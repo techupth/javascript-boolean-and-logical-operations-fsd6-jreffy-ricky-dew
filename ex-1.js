471b37e5521638bd1d3127474b2e9b3c68cca1d0
Jeffiros <krittiwat.c@gmail.com>
2024-05-09 15:19:41 +0700

    feat: fill up answer

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,29 @@
-let isOver18;
-let hasCriminalBlacklist;
+// สมมติว่าเราเขียนโปรแกรมในการตรวจสอบผู้คนที่เข้ามาร่วมงาน Event แห่งหนึ่ง
+
+// เงื่อนไขในการเข้าร่วมก็คือ
+
+// ผู้เข้าร่วมงานต้องมีอายุมากกว่า 18 ปีขึ้นไป
+// และต้องไม่เคยมีประวัติอาชญากรรมมาก่อน
+// โดยที่โปรแกรมจะ Declare ตัว Variable ไว้ให้แล้วดังนี้
+
+// Variable isOver18 เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้เข้าร่วมงานมีอายุมากกว่า 18 ปีขึ้นไป
+// Variable hasCriminalBlacklist เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ร่วมงานเคยมีประวัติอาชญากรรม
+// Variable isAllow เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้เข้าร่วมงานมีสิทธิ์เข้าร่วมงาน
+// โดยผู้ร่วมงานคนหนึ่งชื่อว่า James มีคุณสมบัติดังนี้
+
+// James มีอายุ 18 ปี
+// James ไม่เคยมีประวัติอาชญากรรม
+// ให้ Assign ค่าให้กับ isOver18 และ hasCriminalBlacklist ตามของคุณสมบัติของ James
+
+// ให้ Reassign ค่าของ isAllow เป็น Expression ของ isOver18 และ hasCriminalBlacklist ตามเงื่อนไขข้างต้น
+
+// หลังจากนั้นลอง console.log ค่าของ isAllow เพื่อดูว่า James มีสิทธิ์เข้างานหรือไม่
+
+let isOver18 = false;
+let hasCriminalBlacklist = false;
 
 let isAllow;
 
-// Start coding here
+isAllow = isOver18&&!hasCriminalBlacklist
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,41 @@
 // Exercise #2: Promotion Conditions
+// สมมุติว่าเราเขียนโปรแกรมในการตรวจสอบสิทธิโปรโมชั่นส่วนลดของผู้ซื้อสินค้า
+
+// ผู้ซื้อจะได้รับโปรโมชั่นถ้ากรณีข้อใดข้อหนึ่งต่อไปนี้เป็นจริง (ไม่จำเป็นต้องเป็นจริงทั้งสองข้อ)
+
+// กรณีที่ 1: ผู้ซื้อมียอดสั่งซื้อสินค้าจากเดือนที่แล้วมากกว่า 4000 บาท และวันที่ซื้อสินค้าเป็นวันจันทร์ถึงวันศุกร์ 
+// และไม่เคยซื้อสินค้าจากหมวดหมู่ IT มาก่อน และไม่เคยเข้าร่วมกิจกรรมมหกรรมลดราคามาก่อน
+// กรณีที่ 2 : ผู้ซื้อเป็น Member ในระดับ Platinum"
+// โดยที่โปรแกรมจะ Declare ตัว Variable ไว้ให้แล้วดังนี้
+
+// Variable lastMonthPaidMoreThan4000 เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ซื้อสินค้ามียอดซื้อทั้งหมดในเดือนที่แล้วมากกว่า 4000 บาท
+// Variable isWeekday เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าวันที่ซื้อสินค้าเป็นวันจันทร์ ถึง วันศุกร์
+// Variable hasBoughtProductFromITCategory เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ซื้อสินค้าเคยซื้อสินค้าในหมวดหมู่ IT
+// Variable hasAttendedDiscountEvent เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ซื้อสินค้าเคยเข้าร่วมกิจกรรมมหกรรมลดราคามาก่อนหน้านี้
+// Variable isPlatinum เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ซื้อสินค้าเป็นสมาชิกในระดับ Platinum
+// Variable hasPromotion เก็บข้อมูลประเภท Boolean โดยจะมีค่าเป็น true ถ้าผู้ซื้อสินค้ามีสิทธิได้รับโปรโมชั่นส่วนลด
+// โดยผู้ซื้อสินค้าคนหนึ่งชื่อว่า John มีคุณสมบัติดังนี้
+
+// มียอดสั่งซื้อเดือนที่แล้ว 4001 บาท
+// ไปซื้อของวันศุกร์
+// ไม่เคยซื้อของในหมวดหมู่ IT มาก่อน
+// เคยเข้าร่วมกิจกรรมมหกรรมลดราคาครั้งที่ 2
+// เป็น Member ระดับ Gold
+// ให้ Assign ตัว Value ให้กับ lastMonthPaidMoreThan4000, isWeekday, hasBoughtProductFromITCategory, 
+// hasAttendedDiscountEvent และ isPlatinum ตามของคุณสมบัติของ John
+
+// ให้ Reassign ค่าของ hasPromotion เป็น Expression ของ Variable อื่นๆ ตามเงื่อนไขข้างต้น 
+// หลังจากนั้นลอง console.log ค่าของ hasPromotion เพื่อดูว่า John มีสิทธิได้รับโปรโมชั่นส่วนลดหรือไม่
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false;
 
 let hasPromotion;
 
+hasPromotion = (lastMonthPaidMoreThan4000&&isWeekday&&!hasBoughtProductFromITCategory&&!hasAttendedDiscountEvent)||isPlatinum;
 console.log(hasPromotion);
